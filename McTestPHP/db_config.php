633ed3172266ab4930e7e7eef8df0828703f94b1
julisarrelli <julisarrelli@gmail.com>
2017-01-10 21:31:42 -0300

    subo los php

---
+++ McTestPHP/db_config.php
@@ -0,0 +1,6 @@
+<?php
+define('DB_USER', "u764839990_js"); // db user
+define('DB_PASSWORD', "julian"); // db password (mention your db password here)
+define('DB_DATABASE', "u764839990_mc"); // database name
+define('DB_SERVER', "localhost"); // db server
+?>
